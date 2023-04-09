7e88d255161c6ca35c054ab40b972945fb05c5e7
Shirlene Alves Nunes de Souza <shirleneans@gmail.com>
2023-04-09 15:45:15 -0300

    Primeiro app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+  const [input, setInput] = useState('');
+  const [tarefas, setTarefas] = useState([
+    'Pagar a conta de luz',
+    'Estudar React JS'
+  ]);
+
+
+
+  function handleRegister(e){
+    e.preventDefault();
+
+    setTarefas([...tarefas, input]);
+    setInput('');
+
+  }
+
+  return(
+    <div>
+      <h1>Cadastrando usuario</h1>
+
+      <form onSubmit={handleRegister}>
+        <label>Nome da tarefa:</label><br/>
+        <input 
+         placeholder="Digite uma tarefa" 
+         value={input}
+         onChange={ (e) => setInput(e.target.value) }
+        /><br/>
+        <button type="submit">Registrar</button>
+      </form>
+
+      <br/><br/>
+
+      <ul>
+        {tarefas.map( tarefa => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
+
     </div>
   );
 }
 
 export default App;
+
+
+
+// import Nome from './components/Nome'
+// import { useState } from 'react';
+
+
+// function App() {
+//    const [aluno, setAluno] = useState('Sujeito Programador')
+  
+//    function handleChangeName(nome){
+//     setAluno(nome)
+//    }
+
+//    return(
+//      <div>
+//         <h1>Componente App</h1><br />
+//         <h2> Olá {aluno} </h2>
+//         <button onClick={ () => handleChangeName('Lucas Oliveira') }>
+//           Mudar Nome
+//         </button>
+//         {/* <Nome aluno="Lucas" idade={30} /> */}
+//         <br/>
+//         <br/>
+//      </div>
+//    );
+// }
+
+// export default App;
+
---
+++ src/components/Nome.js
@@ -0,0 +1,8 @@
+function Nome(props){
+    return(
+      <span>Bem-vindo: {props.aluno} idade : {props.idade} </span>
+    )
+     
+  }
+
+  export default Nome;
